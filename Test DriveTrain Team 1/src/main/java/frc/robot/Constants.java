84c002c1df3a6fb4d94af4e4d7b1f6e543e8a95f
rsrohinsood <69406769+rsrohinsood@users.noreply.github.com>
2021-09-23 16:44:00 -0700

    Declared 4 motor ports in Constants.java

--- Test DriveTrain Team 1/src/main/java/frc/robot/Constants.java
+++ Test DriveTrain Team 1/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,8 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int leftFront = 24, leftBack = 13, rightFront = 23, rightback = 15;
+    
+
+}

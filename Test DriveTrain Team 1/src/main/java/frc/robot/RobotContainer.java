c8418254422efc1c00dde798ad33992bb3c93be2
rsrohinsood <69406769+rsrohinsood@users.noreply.github.com>
2021-09-28 16:52:23 -0700

    Added ArcadeJava command to RobotContainer.java

--- Test DriveTrain Team 1/src/main/java/frc/robot/RobotContainer.java
+++ Test DriveTrain Team 1/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,13 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +22,18 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  public final DriveTrain m_drive = new DriveTrain();
+  public XboxController m_xboxController = new XboxController(0);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
+
+    
+
+    m_drive.setDefaultCommand(
+      new ArcadeDrive(m_drive, () -> m_xboxController.getY(Hand.kLeft), () -> m_xboxController.getX(Hand.kRight)));
   }
 
   /**
@@ -41,8 +49,10 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+  public DriveTrain getDrivetrain() {
+    return m_drive;
   }
+
+  
+
 }
